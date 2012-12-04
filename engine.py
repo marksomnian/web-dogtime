99304dd631286d784d820666b0949177d8db6cae
Marks Polakovs <laboratoriesit@gmail.com>
2012-12-04 20:46:33 +0100

    Some kind of code

---
+++ engine.py
@@ -0,0 +1,11 @@
+class dogEngine:
+
+	def getLastDogEvent():
+	
+	def setNewDogEvent(user, isStart):
+		import mysql.connector
+		cnx = mysql.connector.connect(user='testing123', password='123',
+                              host='127.0.0.1',
+                              database='dogtime_dev')
+		cnx.close()
+		
