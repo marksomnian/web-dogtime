eff44433a9adad04d6018f978ad60aa88831c436
Marks Polakovs <laboratoriesit@gmail.com>
2012-12-11 20:49:15 +0100

    Did some work,
    Messed with MYSQL

--- engineSetDogEvent.py
+++ engineSetDogEvent.py
@@ -1,15 +1,13 @@
 import sys, mysql.connector
-dogWalker = sys.argv[1]
-startWalkArg = sys.argv[2]
-if (startWalkArg == 'true'):
-    startWalk = True
-else:
-    startWalk = False
 
-print dogWalker, startWalk
+userId = sys.argv[1]
+startDateTime = sys.argv[2]
+endDateTime = sys.argv[3]
+
+print userId, startDateTime, endDateTime
 
 cnx = mysql.connector.connect(user='testing123', password='123',
 						host='127.0.0.1',
 						database='dogtime_dev')
-
 cnx.close()
+
